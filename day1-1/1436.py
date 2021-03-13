9c1460d18f3e45c2678faf4a072538c4b6919931
healthcarefrontend <himchan9494@gmail.com>
2021-03-13 11:15:38 +0900

    20210313_01

--- day1-1/1436.py
+++ day1-1/1436.py
@@ -1,9 +1,23 @@
 # 영화감독 숌
-a = int(input())
-b = str(a - 1)
+n = int(input())
+cnt = 0
+six_n = 666
+while True:
+    if '666' in str(six_n):
+        cnt += 1
+    if cnt == n:
+        print(six_n)
+        break
+    print(type(six_n))
+    six_n += 1
 
-if b == '0':
-    print('666')
 
-else:
-    print(b+'666')
+# 오류코드
+# a = int(input())
+# b = str(a - 1)
+
+# if b == '0':
+#     print('666')
+
+# else:
+#     print(b+'666')
